7ec16bc918645d74b817d834878ff0031b393818
BrianPhiri <brianphiri.9523@gmal.com>
2017-01-11 08:32:37 +0300

    Added Patients Models (App/Models/PatientModels)

---
+++ lib/app/Models/PatientModels/Dependant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\PatientModels;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dependant extends Model
+{
+
+}
---
+++ lib/app/Models/PatientModels/Visit.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\PatientModels;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Visit extends Model
+{
+
+}
