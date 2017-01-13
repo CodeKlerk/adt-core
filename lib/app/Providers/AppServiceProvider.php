09a96800055344f121a9decc4d75dc289e96c612
BrianPhiri <brianphiri.9523@gmal.com>
2017-01-13 07:07:41 +0300

    configuring the server for CORS

--- lib/app/Providers/AppServiceProvider.php
+++ lib/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +15,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+    	/** @var \Illuminate\Http\Request $request */
+        $request = $this->app->make('request');
+        if($request->isMethod('OPTIONS')) {
+		    $this->app->options($request->path(), function(){
+			    return response('OK', 200);
+		    });
+	    }
     }
 }
