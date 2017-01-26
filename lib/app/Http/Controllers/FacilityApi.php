78327d1ebe039b7e55d42f93724ea405732fc1e4
BrianPhiri <brianphiri.9523@gmal.com>
2017-01-26 10:15:16 +0300

    Added facility update

--- lib/app/Http/Controllers/FacilityApi.php
+++ lib/app/Http/Controllers/FacilityApi.php
@@ -34,5 +34,33 @@ class FacilityApi extends Controller
             return $this->response->errorBadRequest();
         }
     }
+
+    public function facilityput($facility_id){
+        $input = Request::all();
+        $facility = Facilities::findOrFail($facility_id);
+        if(!$facility){
+            return $this->response->errorNotFound();
+        }else{
+            $facility->update([
+                'code' => $input['code'],
+                'name' => $input['name'],
+                'postal_address' => $input['postal_address'],
+                'email_address' => $input['email_address'],
+                'phone_number' => $input['phone_number'],
+                'adult_age' => $input['adult_age'],
+                'service' => $input['service'],
+                'weekday_max' => $input['weekday_max'],
+                'weekend_max' => $input['weekend_max'],
+                'county_id' => $input['county_id'],
+                'county_sub_id' => $input['county_sub_id'],
+                'supporter_id' => $input['supporter_id']
+            ]);
+            if($facility->save()){
+                return response()->json(['msg'=> 'updated facility']);
+            }else{
+                return $this->response->errorBadRequest();
+            }
+        }
+    }
     
 }
