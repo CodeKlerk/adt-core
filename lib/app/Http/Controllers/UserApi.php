87765e3cd2b5705b6720f14b208a9833fdaf59dd
BrianPhiri <brianphiri.9523@gmal.com>
2017-02-07 09:56:20 +0300

    Added dif password for new user

--- lib/app/Http/Controllers/UserApi.php
+++ lib/app/Http/Controllers/UserApi.php
@@ -49,7 +49,7 @@ class UserApi extends Controller
             'access_level_id' => $input['access_level_id'],
             'facility_id' => $input['facility_id'],
             'created_by_id' => $input['created_by_id'],
-            'password' => app('hash')->make($input['password']),
+            'password' => app('hash')->make($input['adt123']),
             'remember_token' => str_random(10)
         ]);
     }
