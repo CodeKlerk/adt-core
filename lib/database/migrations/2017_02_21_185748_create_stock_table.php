58b4f8deb6aadf9a78049766b9e3f647dd0b31d0
BrianPhiri <brianphiri.9523@gmal.com>
2017-02-21 22:15:35 +0300

    created migrations for stock and stock item

---
+++ lib/database/migrations/2017_02_21_185748_create_stock_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_stock', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp('transaction_time');
+            $table->text('transaction_detail');
+            $table->string('ref_number');
+            $table->integer('user_id')->unsigned();
+            $table->integer('store_id')->unsigned();
+            $table->integer('facility_id')->unsigned();
+            $table->integer('transaction_type_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+            // fk
+            $table->foreign('user_id')->references('id')->on('tbl_users');
+            $table->foreign('store_id')->references('id')->on('tbl_store');
+            $table->foreign('facility_id')->references('id')->on('tbl_facility');
+            $table->foreign('transaction_type_id')->references('id')->on('tbl_transaction_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_stock');
+    }
+}
---
+++ lib/database/migrations/2017_02_21_185810_create_stock_item_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_stock_item', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('batch_number');
+            $table->date('expiry_date');
+            $table->integer('quantity_in');
+            $table->integer('quantity_out');
+            $table->integer('quantity_packs');
+            $table->integer('balance_before');
+            $table->integer('balance_after');
+            $table->double('unit_cost');
+            $table->double('total_cost');
+            $table->text('comment');
+            $table->integer('drug_id')->unsigned();
+            $table->integer('stock_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+            // fk
+            $table->foreign('drug_id')->references('id')->on('tbl_drug');
+            $table->foreign('stock_id')->references('id')->on('tbl_stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_stock_item');
+    }
+}
