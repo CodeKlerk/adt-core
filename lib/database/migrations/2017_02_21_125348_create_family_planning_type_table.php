c4d0d8027e95ea80ff9aed4033b0d67625add3ce
BrianPhiri <brianphiri.9523@gmal.com>
2017-02-21 15:58:35 +0300

    made migrations for lookup tables

---
+++ lib/database/migrations/2017_02_21_125348_create_family_planning_type_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFamilyPlanningTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_family_planning', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_family_planning');
+    }
+}
