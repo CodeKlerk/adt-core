b274cbd108d6d4f02b39d605513eb59aa69831b8
BrianPhiri <brianphiri.9523@gmal.com>
2017-02-21 21:45:02 +0300

    created migrations for store

---
+++ lib/database/migrations/2017_02_21_184203_create_store_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_store', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->enum('type', ['store', 'pharmacy']);
+            $table->integer('facility_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+
+            // fk
+            $table->foreign('facility_id')->references('id')->on('tbl_facility');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_store');
+    }
+}
