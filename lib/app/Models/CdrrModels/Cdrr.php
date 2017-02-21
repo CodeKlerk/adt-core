13f32bdf87e81e7eab4e557e0580b4366986c903
BrianPhiri <brianphiri.9523@gmal.com>
2017-02-21 20:18:38 +0300

    response in cdrr

--- lib/app/Models/CdrrModels/Cdrr.php
+++ lib/app/Models/CdrrModels/Cdrr.php
@@ -14,8 +14,24 @@ class Cdrr extends Model
                             'comments', 'reports_expected', 'reports_actual', 
                             'services', 'is_non_arv', 'facility_id', 'supporter_id' ];
     protected $dates = ['deleted_at'];
+    protected $hidden = [ 'created_at', 'updated_at', 'deleted_at', 'facility_id', 'supporter_id', 'supporter', 'facility'];
+    protected $appends = array('facility_name', 'supporter_name');
 
     public function facility(){
         return $this->belongsTo('App\Models\FacilityModels\Facilities', 'facility_id');
+    }
+    public function supporter(){
+        return $this->belongsTo('App\Models\ListsModels\Supporter', 'supporter_id');
+    }
+
+    public function getFacilityNameAttribute(){
+        $facility_name = null;
+        if($this->facility){ $facility_name = $this->facility->name; }
+        return $facility_name;
+    }
+    public function getSupporterNameAttribute(){
+        $drug_supporter = null;
+        if($this->supporter){ $drug_supporter = $this->supporter->name; }
+        return $drug_supporter;
     }
 }
