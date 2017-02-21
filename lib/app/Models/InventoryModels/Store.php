4950e27664f1b2dd031e318fda2b5e9ef8c3d182
BrianPhiri <brianphiri.9523@gmal.com>
2017-02-21 21:51:37 +0300

    hide columns in store

--- lib/app/Models/InventoryModels/Store.php
+++ lib/app/Models/InventoryModels/Store.php
@@ -11,4 +11,17 @@ class Store extends Model
     protected $table = 'tbl_store';
     protected $fillable = ['name', 'type','facility_id'];
     protected $dates = ['deleted_at'];
+    protected $hidden = ['created_at', 'updated_at', 'facility_id'];
+    protected $appends = array('facility_name');
+
+    public function facility(){
+        return $this->belongsTo('App\Models\FacilityModels\Facilities', 'facility_id');
+    }
+
+    public function getFacilityNameAttribute(){
+        $facility_name = null;
+        if($this->facility){ $facility_name = $this->facility->name; }
+        return $facility_name;
+    }
+
 }
