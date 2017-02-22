b5b96b70c19248f2bad475680e4d11cede0045ae
BrianPhiri <brianphiri.9523@gmal.com>
2017-02-22 13:56:50 +0300

    hide columns in patient famillyplanning

--- lib/app/Models/PatientModels/PatientFamilyPlanning.php
+++ lib/app/Models/PatientModels/PatientFamilyPlanning.php
@@ -8,8 +8,20 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class PatientFamilyPlanning extends Model
 {
     use SoftDeletes;
-    
+
     protected $table = 'tbl_patient_family_planning';
     protected $fillable = ['patient_id', 'family_planning_id'];
     protected $dates = ['deleted_at'];
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+    protected $appends = array('family_plan_name');
+
+    public function familyplan(){
+        return $this->belongsTo('App\Models\ListsModels\Familyplanning', 'family_planning_id');
+    }
+
+    public function getFamilyPlanNameAttribute(){
+        $family_plan_name = null;
+        if($this->familyplan){ $family_plan_name = $this->familyplan->name; }
+        return $family_plan_name;
+    }
 }
