dc5436e5f9de07090f66933fe16001a94dae423d
BrianPhiri <brianphiri.9523@gmal.com>
2017-02-22 11:38:00 +0300

    hide columns in patietns allergy

--- lib/app/Models/PatientModels/PatientAllergies.php
+++ lib/app/Models/PatientModels/PatientAllergies.php
@@ -12,5 +12,16 @@ class PatientAllergies extends Model
     protected $table = 'tbl_patient_drug_allergy'; 
     protected $fillable = ['patient_id','drug_id'];
     protected $dates = ['deleted_at'];
-    
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at', 'drug'];
+    protected $appends = array('drug_name');
+
+    public function drug(){
+        return $this->belongsTo('App\Models\DrugModels\Drug');
+    }
+
+    public function getDrugNameAttribute(){
+        $drug_name = null;
+        if($this->drug){ $drug_name = $this->drug->name; }
+        return $drug_name;
+    }
 }
