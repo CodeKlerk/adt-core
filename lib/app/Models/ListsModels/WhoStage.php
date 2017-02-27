d00687b079e01506903b3402eec6a83cebc89da9
codeklerk <billnguts@gmail.com>
2017-02-27 12:35:06 +0300

    Whostage.php >> WhoStage.php

--- lib/app/Models/ListsModels/WhoStage.php
+++ lib/app/Models/ListsModels/WhoStage.php
@@ -5,7 +5,7 @@ namespace App\Models\ListsModels;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Whostage extends Model
+class WhoStage extends Model
 {
     use SoftDeletes;
 
