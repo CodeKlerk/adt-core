363d3b5621bd754728d1d7f91a50d7c71af4be39
BrianPhiri <brianphiri.9523@gmal.com>
2017-02-27 12:37:03 +0300

    maps structure update

--- lib/app/Models/MapsModels/Maps.php
+++ lib/app/Models/MapsModels/Maps.php
@@ -13,8 +13,24 @@ class Maps extends Model
     protected $fillable = ['status', 'code', 'period_begin', 'period_end', 'reports_expected', 
                             'reports_actual', 'services', 'comments', 'facility_id', 'supporter_id'];
     protected $dates = ['deleted_at'];
-    
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at', 'facility_id', 'supporter_id', 'facility', 'supporter'];
+    protected $appends = array('facility_name', 'supporter_name');
+
     public function facility(){
         return $this->belongsTo('App\Models\FacilityModels\Facilities', 'facility_id');
+    }
+    public function supporter(){
+        return $this->belongsTo('App\Models\ListsModels\Supporter', 'supporter_id');
+    }
+
+    public function getFacilityNameAttribute(){
+        $facility_name = null;
+        if($this->facility){ $facility_name = $this->facility->name; }
+        return $facility_name;
+    }
+    public function getSupporterNameAttribute(){
+        $drug_supporter = null;
+        if($this->supporter){ $drug_supporter = $this->supporter->name; }
+        return $drug_supporter;
     }
 }
